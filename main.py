132bf0fc80bfa3825d993c97e187f2f91a0f65bd
Daniel Mauricio Vargas Palomeque <var28790@ttu.edu>
2026-04-10 14:44:09 -0500

    Data Retrieval & RW Matrix

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
-# # This is a sample Python script.
-#
-# # Press Shift+F10 to execute it or replace it with your code.
-# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 import argparse
+import sys
 
-parser = argparse.ArgumentParser()
+CELL_TYPES = {'O', 'X', 'o', 'x', '.'}
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Serial Cellular Life Simulator")
+
+    parser.add_argument('-i', type=str, required=True, help="Path to the starting cellular matrix input file.")
+    parser.add_argument('-o', type=str, required=True, help="Path for the final output file.")
+    parser.add_argument('-p', type=int, default=1, help="Number of processes to spawn (default: 1).")
+
+    args = parser.parse_args()
+
+    return args
+
+def read_matrix(path):
+    matrix = []
+    with open(path) as file:
+        row = file.readline().strip()
+
+        for cell in row:
+            if cell not in CELL_TYPES:
+                print(f"Invalid cell type: '{cell}'")
+                sys.exit(1)
+
+        matrix.append(list(row))
+
+        side = len(row)
+
+        for line in file:
+            row = line.strip()
+
+            if len(row) != side:
+                print(f"Invalid row length: '{len(row)}'")
+
+            for cell in row:
+                if cell not in CELL_TYPES:
+                    print(f"Invalid cell type: '{cell}'")
+                    sys.exit(1)
+
+            matrix.append(list(row))
+
+
+    return matrix
+
+def write_matrix(matrix, path):
+    with open(path, 'w') as file:
+        for row in matrix:
+            file.write("".join(row) + "\n")
+
+def main():
+    # args = parse_args()
+
+    # matrix = read_matrix(arg.i)
+    matrix = read_matrix("ten_by_ten/time_step_0.dat")
+
+    # write_matrix(matrix, arg.o)
+    write_matrix(matrix, "test_output.txt")
 
 if __name__ == '__main__':
-    print('Hi')
+    main()
