9b3cebaaf24a40a18b5ffca92fc67fd717cf1ecb
Daniel Mauricio Vargas Palomeque <var28790@ttu.edu>
2026-04-30 09:27:17 -0500

    Global ruleCreation() and STAGE_UPDATE
    Refactor matrix reading and simulation functions for clarity and efficiency. Consolidate matrix initialization and remove redundant code.

--- Daniel_Vargas_R11998328_final_project.py
+++ Daniel_Vargas_R11998328_final_project.py
@@ -14,79 +14,20 @@ import argparse
 import copy
 import os
 import sys
+from multiprocessing import Pool
 
+# Constants and Sets
 POWERS_OF_TWO = {1, 2, 4, 8, 16, 32, 64}
 FIBONACCI_SET = {0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89}
 PRIME_NUMBERS = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89}
-STAGE_UPDATE = {3: {}, 1: {}, 0: {}, -1: {}, -3: {}}
 
 # 1.3.1 Symbols
 # 1.3.2.2 Value assigned based on state
 CELL_TO_VALUE = {"O": 3, "o": 1, ".": 0, "x": -1, "X": -3}
 VALUE_TO_CELL = {3: "O", 1: "o", 0: ".", -1: "x", -3: "X"}
 
-
-# Marix read from input file
-def readMatrix(inputFile):
-    matrix = []
-    with open(inputFile) as file:
-        # with open("ten_by_ten/time_step_0.dat") as file:
-        # Get first line in file
-        file_line = file.readline().strip()
-
-        # Adding dead cells to the row start
-        row = [0] * 2
-
-        # Getting all cells in row from file
-        for cell in file_line:
-            if cell in CELL_TO_VALUE:
-                row.append(CELL_TO_VALUE[cell])
-            elif cell != "\n":
-                print(f"Invalid cell type: '{cell}'")
-                sys.exit(1)
-
-        # Adding dead cells to the row end
-        row.append(0)
-        row.append(0)
-
-        # Get constant length for all rows
-        cols = len(row)
-
-        # Adding upper dead cells to the matrix
-        matrix.append([0] * cols)
-        matrix.append([0] * cols)
-
-        # Adding the first row of the matrix
-        matrix.append(row)
-
-        # Get next lines in file
-        for file_line in file:
-            line = file_line.strip()
-
-            row = [0] * 2
-
-            # Check if rows are all equal length
-            if len(line) != (cols - 4):
-                print(f"Invalid row length: '{len(line)}'")
-
-            for cell in file_line:
-                if cell in CELL_TO_VALUE:
-                    row.append(CELL_TO_VALUE[cell])
-                elif cell != "\n":
-                    print(f"Invalid cell type: '{cell}'")
-                    sys.exit(1)
-
-            # Adding rows with cells to matrix
-            matrix.append(row + [0] * 2)
-
-        # Adding lower dead cells to the matrix
-        matrix.append([0] * cols)
-        matrix.append([0] * cols)
-
-        # Get constant height for matrix
-        rows = len(matrix)
-
-        return [matrix, cols, rows]
+# Global look-up table
+STAGE_UPDATE = {3: {}, 1: {}, 0: {}, -1: {}, -3: {}}
 
 
 # Create update dictionary
@@ -139,11 +80,17 @@ def ruleCreation():
             else:
                 STAGE_UPDATE[-3][inner_score][outer_score] = -3
 
+# 1.3.2 Iterative Rules
+ruleCreation()
+
 
 # Life simulation function
-def simulation(matrix, sim_matrix):
-    cols = len(matrix)
-    rows = len(matrix[0])
+def chunk_simulation(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Matrix copy to work on
+    sim_matrix = copy.deepcopy(matrix)
 
     # Matrix iteration
     for y in range(2, rows - 2):
@@ -167,7 +114,7 @@ def simulation(matrix, sim_matrix):
                 # low_row
                     low_row[x_left1] + low_row[x] + low_row[x_right1] +
                     # self_row
-                    self_row[x_left1] + self_row[x_right1] +
+                    self_row[x _left1] + self_row[x_right1] +
                     # high_row
                     high_row[x_left1] + high_row[x] + high_row[x_right1]
             )
@@ -190,9 +137,8 @@ def simulation(matrix, sim_matrix):
 
             # Updating the cell based on the simulation rules
             sim_matrix[y][x] = STAGE_UPDATE[self_row[x]][inner_score][outer_score]
-
-    # Updating the base matrix
-    matrix = copy.deepcopy(sim_matrix)
+    
+    return sim_matrix[2 : rows-2]
 
 
 # Main driver function
@@ -228,10 +174,59 @@ def main():
         sys.exit(1)
 
     # 1.2.1 Read Matrix
-    matrix, cols, rows = readMatrix(args.i)
+    matrix = []
+    with open(args.i) as file:
+        # with open("ten_by_ten/time_step_0.dat") as file:
+        # Get first line in file
+        file_line = file.readline().strip()
 
-    # 1.3.2 Iterative Rules
-    ruleCreation()
+        # Adding dead cells to the row start
+        row = [0] * 2
+
+        # Getting all cells in row from file
+        for cell in file_line:
+            if cell in CELL_TO_VALUE:
+                row.append(CELL_TO_VALUE[cell])
+            elif cell != "\n":
+                print(f"Invalid cell type: '{cell}'")
+                sys.exit(1)
+
+        # Adding dead cells to the row end
+        row.extend([0, 0])
+
+        # Get constant length for all rows
+        cols = len(row)
+
+        # Adding the first row of the matrix
+        matrix.append(row)
+
+        # Get next lines in file
+        for file_line in file:
+            line = file_line.strip()
+
+            # Check if rows are all equal length
+            if len(line) != (cols - 4):
+                print(f"Invalid row length: '{len(line)}'")
+
+            row = [0] * 2
+
+            for cell in file_line:
+                if cell in CELL_TO_VALUE:
+                    row.append(CELL_TO_VALUE[cell])
+                elif cell != "\n":
+                    print(f"Invalid cell type: '{cell}'")
+                    sys.exit(1)
+
+            row.extend([0, 0])
+            
+            # Adding rows with cells to matrix
+            matrix.append(row)
+
+        # Adding lower dead cells to the matrix
+        matrix = [[0] * cols, [0] * cols] + matrix + [[0] * cols, [0] * cols]
+
+        # Get constant height for matrix
+        rows = len(matrix)
 
     # 1.3 Matrix Processing
     # Storage matrix for end of iteration
